d03f5ded275bcf8389fcf5ff3a099d061f84ac0c
ZidaneCode <redjdalzidane01@gmail.com>
2025-03-02 23:14:47 +0100

    Initialize project structure with service, model, controller, repository packages and H2 database with data.sql

---
+++ src/main/java/com/myProject/api/controller/EmployeeController.java
@@ -0,0 +1,27 @@
+package com.myProject.api.controller;
+
+import com.myProject.api.model.Employee;
+import com.myProject.api.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    /**
+     * Read - Get all employees
+     * @return - An Iterable object of Employee full filled
+     */
+    @GetMapping("/employees")
+    public Iterable<Employee> getEmployees() {
+        return employeeService.getEmployees();
+    }
+
+
+}
